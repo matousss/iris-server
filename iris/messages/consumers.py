20086413f997b02e87ce440ba238c1ffa8e28234
matousss <matousss33@gmail.com>
2022-04-23 17:20:19 +0200

    TODO

--- iris/messages/consumers.py
+++ iris/messages/consumers.py
@@ -33,6 +33,7 @@ def message_update(sender, instance: Message, created=False, **kwargs):
 
 
 # todo disconnect on token invalidation
+# todo updated channels
 class MessageConsumer(WebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(args, kwargs)
