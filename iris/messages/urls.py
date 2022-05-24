23c5392a74d8ad2357477668a44a34e7b7c3a5ee
matousss <matousss33@gmail.com>
2022-05-24 20:46:32 +0200

    Removed some useless imports

--- iris/messages/urls.py
+++ iris/messages/urls.py
@@ -1,13 +1,4 @@
-import glob
-import imghdr
-import os
-from mimetypes import guess_type
-
-from django.conf import settings
-from django.contrib.auth import authenticate
-from django.http import FileResponse, HttpResponseNotFound, HttpResponseForbidden, Http404
 from django.urls import re_path, path, include
-from rest_framework.generics import RetrieveAPIView, GenericAPIView
 from rest_framework.routers import DefaultRouter
 
 from .consumers import MessageConsumer
@@ -21,14 +12,10 @@ router.register(r'message', MessageViewSet, basename='message')
 
 
 urlpatterns = [
-    # path('media/<str:channel_id>/<str:message_id>/<str:file>', GetMedia.as_view()),
-    # path('api/channel/<uuid:channel_id>', ChannelAPIView.as_view()),
-    # path('api/channel', GetChannelsAPI.as_view()),
     path('api/', include(router.urls)),
     path(r'api/viewed-channel/<uuid:channel_id>', ViewedChannelAPI.as_view(), name='viewed-channel'),
 ]
 
 ws_urlpatterns = [
-    # re_path(r'^ws/client/(?P<room_id>[^/]+)/$', ClientConsumer.as_asgi()),
     re_path(r'ws/messages', MessageConsumer.as_asgi())
 ]
